1b5f56a3f8c3aea55d25b22fc1cb0d9b46a22361
basherdasher <bogachevadasha1997@mail.ru>
2021-09-26 01:06:28 +0300

    Counter with for instead of while

--- homework/ex3. InpOutp/charCounter.c
+++ homework/ex3. InpOutp/charCounter.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-// count input symbols v1
+// count input symbols v2
 main()
 {
-	long nc;
-	nc = 0;
-
-	while (getchar() != EOF)
-		++nc;
-	printf("\n%ld\n", nc);
+	double nc;
+	
+	for (nc = 0; getchar() != EOF; ++nc)
+	;
+	printf("\n%.0f\n", nc);
 }
